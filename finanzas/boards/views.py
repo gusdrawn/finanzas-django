fd3ec8103211ffddefb30cb0c0d9d2176fb83a26
Gustavo Guillen <4174357+gusdrawn@users.noreply.github.com>
2024-04-21 21:03:01 -0400

    Fix edit posts for no-owner

--- finanzas/boards/views.py
+++ finanzas/boards/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Count
 from django.utils import timezone
 from django.views.generic import UpdateView
+from django.utils.decorators import method_decorator
 
 @login_required
 def home(request):
@@ -63,13 +64,17 @@ def reply_topic(request, pk, topic_pk):
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
 
-
+@method_decorator(login_required, name='dispatch')
 class PostUpdateView(UpdateView):
     model = Post
     fields = ('message', )
     template_name = 'edit_post.html'
     pk_url_kwarg = 'post_pk'
     context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
 
     def form_valid(self, form):
         post = form.save(commit=False)
